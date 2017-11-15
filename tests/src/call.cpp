a2b7b0110a3e987f0fdfbd62b0028e1043f003cc
Rich Halliday <rihallid@microsoft.com>
2017-11-15 10:25:46 -0800

    Removed debugging line in test.

--- tests/src/call.cpp
+++ tests/src/call.cpp
@@ -3,8 +3,6 @@
 #include "../../src/cpu.h"
 #include "../../src/op_codes.h"
 
-#include <iostream>
-
 using namespace Catch;
 
 TEST_CASE("Testing call op codes")
@@ -14,7 +12,6 @@ TEST_CASE("Testing call op codes")
   SECTION("A program can load a subroutine")
   {
     uint8_t program[3] = { CALL, 0x3b, 0x08 };
-    std::cout << static_cast<void *>(program);
 
     cpu.processProgram(program, 3);
 
